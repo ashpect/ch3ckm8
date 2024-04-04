3dd59700a39b28900038c4b8d6bf933aafe99c61
Praneeth Sarode <praneethsarode@gmail.com>
2024-04-04 06:22:08 +0530

    Add helper functions
    Signed-off-by: Praneeth Sarode <praneethsarode@gmail.com>

--- engine/bitBoard.go
+++ engine/bitBoard.go
@@ -138,6 +138,38 @@ func (b *Board) getPieceType(pos uint64) PieceType {
 	return 0
 }
 
+func getPieceNotation(pieceType PieceType,isWhite bool) string {
+
+	var base_string string = ""
+	switch pieceType {
+	case Pawn:
+		base_string = ""
+	case Knight:
+		base_string="N"
+	case Bishop:
+		base_string="B"
+	case Rook:
+		base_string="R"
+	case Queen:
+		base_string="Q"
+	case King:
+		base_string="K"
+	}
+	return base_string	
+}
+
+func getPosNotation(pos uint64) string {
+	var posNotation string = ""
+	var i uint64
+	for i = 0x8000000000000000; i > 0; i >>= 1 {
+		if pos&i != 0 {
+			posNotation = string(97 + (63 - i) % 8) + string(49 + int((63 - i) / 8))
+			break
+		}
+	}
+	return posNotation
+}
+
 func (b *Board) isCheck(isWhite bool) bool {
 	oppMoves := b.getAllMoves(!isWhite)
 	var kingPos uint64
