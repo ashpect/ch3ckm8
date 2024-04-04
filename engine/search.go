b13f06a6b4d65ba3e433f60fa3e1cd894e4788b7
ashpect <ashishndiitr@gmail.com>
2024-04-04 05:56:19 +0530

    minmax

--- engine/search.go
+++ engine/search.go
@@ -1,6 +1,6 @@
 package engine
 
-func (b *Board) alphaBetaMiniMax(isWhite bool, depth int, alpha, beta float64) (float64, [2]uint64) {
+func (b *Board) alphaBetaMiniMax(isWhite bool, depth int) (float64, [2]uint64) {
 	if depth == 0 || b.isCheckmate() {
 		a, c := b.eval()
 		return a + c, [2]uint64{0, 0}
@@ -10,7 +10,7 @@ func (b *Board) alphaBetaMiniMax(isWhite bool, depth int, alpha, beta float64) (
 		bestmove := [2]uint64{0, 0}
 		for _, move := range b.getAllLegalMoves(isWhite) {
 			wasPieceCaptured, capturedPieceType := b.makeMove(move[0], move[1], isWhite, b.getPieceType(move[0]))
-			eval, _ := b.alphaBetaMiniMax(!isWhite, depth-1, alpha, beta)
+			eval, _ := b.alphaBetaMiniMax(!isWhite, depth-1)
 			b.unmakeMove(move[0], move[1], isWhite, wasPieceCaptured, capturedPieceType)
 			if eval > maxEval {
 				maxEval = eval
@@ -24,10 +24,10 @@ func (b *Board) alphaBetaMiniMax(isWhite bool, depth int, alpha, beta float64) (
 		bestmove := [2]uint64{0, 0}
 		for _, move := range b.getAllLegalMoves(isWhite) {
 			wasPieceCaptured, capturedPieceType := b.makeMove(move[0], move[1], isWhite, b.getPieceType(move[0]))
-			eval, _ := b.alphaBetaMiniMax(!isWhite, depth-1, alpha, beta)
+			eval, _ := b.alphaBetaMiniMax(!isWhite, depth-1)
 			b.unmakeMove(move[0], move[1], isWhite, wasPieceCaptured, capturedPieceType)
 			if eval < minEval {
-				beta = eval
+				minEval = eval
 				bestmove = move
 			}
 		}
