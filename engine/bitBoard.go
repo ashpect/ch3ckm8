c2bfccf72c3492a388a36b6e412f76f5aba90ccc
ashpect <ashishndiitr@gmail.com>
2024-04-04 06:38:27 +0530

    updates

--- engine/bitBoard.go
+++ engine/bitBoard.go
@@ -2,7 +2,6 @@ package engine
 
 import (
 	"fmt"
-	"strings"
 )
 
 // Iinitializes the chess board with the starting positions of the pieces.
@@ -137,47 +136,6 @@ func (b *Board) getPieceType(pos uint64) PieceType {
 		return King
 	}
 	return 0
-}
-
-func (b *Board) getPieceNotation(pos uint64) string {
-
-	pieceType := b.getPieceType(pos)
-	isWhite := b.getColour(pos)
-	var base_string string = ""
-	switch pieceType {
-	case Pawn:
-		base_string = ""
-	case Knight:
-		base_string = "N"
-	case Bishop:
-		base_string = "B"
-	case Rook:
-		base_string = "R"
-	case Queen:
-		base_string = "Q"
-	case King:
-		base_string = "K"
-	}
-	if !isWhite {
-		base_string = strings.ToLower(base_string)
-	}
-	return base_string
-}
-
-func (b *Board) getColour(pos uint64) bool {
-	return pos&b.whitePieces != 0
-}
-
-func (b *Board) getPosNotation(pos uint64) string {
-	var posNotation string = ""
-	var i uint64
-	for i = 0x8000000000000000; i > 0; i >>= 1 {
-		if pos&i != 0 {
-			posNotation = string(rune(97+(63-i)%8)) + string(rune(49+int((63-i)/8)))
-			break
-		}
-	}
-	return posNotation
 }
 
 func (b *Board) isCheck(isWhite bool) bool {
